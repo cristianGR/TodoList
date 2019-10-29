bbff022caf684bb402fc5a1f96194ae269b6ca00
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-28 21:57:23 -0300

    todolist

---
+++ todoList/todoList/Model/DataStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoList.Model
+{
+    class DataStore
+    {
+        private ObservableCollection<Task> tasks;
+    
+        public DataStore(){
+            tasks = new ObservableCollection<Task>(); 
+        }
+
+        public void Add(string nombre) {
+            tasks.Add(new Task { Name = nombre });
+        }
+
+        public ObservableCollection<Task> Tasks
+        {
+            get { return tasks; }
+        }
+
+    }
+}
